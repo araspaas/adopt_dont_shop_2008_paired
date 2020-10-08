20b1b1ad26b6438cc5cd9b1a452556c7d4154b87
Austin Aspaas <araspaas@live.com>
2020-10-07 19:42:28 -0600

    Add Edit and update actions to the shelters controller

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -17,6 +17,16 @@ class SheltersController < ApplicationController
     redirect_to '/shelters'
   end
 
+  def edit
+    @shelter = Shelter.find(params[:id])
+  end
+
+  def update
+    shelter = Shelter.find(params[:id])
+    shelter.update(shelter_params)
+    redirect_to "/shelters/#{shelter.id}"
+  end
+
   def shelter_params
     params.permit(:name, :address, :city, :state, :zip)
   end
