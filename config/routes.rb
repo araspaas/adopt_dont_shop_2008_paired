3edef5c36f92677bd02fab798c6c8c3fff5fa908
Austin Aspaas <araspaas@live.com>
2020-10-09 21:11:18 -0600

    Add edit pet routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,6 @@ Rails.application.routes.draw do
   get '/pets/:id', to: 'pets#show'
   get 'shelters/:id/pets/new', to: 'pets#new'
   post '/shelters/:id/pets', to: 'pets#create'
+  get '/pets/:id/edit', to: 'pets#edit'
+  patch '/pets/:id', to: 'pets#update'
 end
