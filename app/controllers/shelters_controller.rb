3b936a5ce664adf1a0f3a3fc74d5a0717bac2516
Austin Aspaas <araspaas@live.com>
2020-10-09 18:50:23 -0600

    Add pets action to the shelters controller

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -32,6 +32,10 @@ class SheltersController < ApplicationController
     redirect_to "/shelters"
   end
 
+  def pets
+    @shelter = Shelter.find(params[:id])
+  end
+
   private
   def shelter_params
     params.permit(:name, :address, :city, :state, :zip)
