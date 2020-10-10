6e01c25027201f21a371edf30342a4d26f60558b
Austin Aspaas <araspaas@live.com>
2020-10-10 13:04:18 -0600

    Add test to allow the user to edit a pet from the pets index page

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -16,25 +16,46 @@ describe "As a visitor" do
       visit "/pets"
 
       expect(page).to have_content(pet1.name)
-      expect(page).to have_content(pet1.image)
       expect(page).to have_content(pet1.age)
       expect(page).to have_content(pet1.sex)
       expect(page).to have_content(shelter_1.name)
       expect(page).to have_content(pet2.name)
-      expect(page).to have_content(pet2.image)
       expect(page).to have_content(pet2.age)
       expect(page).to have_content(pet2.sex)
       expect(page).to have_content(shelter_2.name)
       expect(page).to have_content(pet3.name)
-      expect(page).to have_content(pet3.image)
       expect(page).to have_content(pet3.age)
       expect(page).to have_content(pet3.sex)
       expect(page).to have_content(shelter_3.name)
       expect(page).to have_content(pet4.name)
-      expect(page).to have_content(pet4.image)
       expect(page).to have_content(pet4.age)
       expect(page).to have_content(pet4.sex)
       expect(page).to have_content(shelter_4.name)
+    end
+    it "I can see a link to edit each pet and use it" do
+      shelter_1 = create(:shelter)
+      shelter_2 = create(:shelter)
+
+      pet1 = shelter_1.pets.create!(name: "Bella", image: "https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/dog_cool_summer_slideshow/1800x1200_dog_cool_summer_other.jpg", age: "5", sex: "Female", description: "Bork", status: 0)
+      pet2 = shelter_2.pets.create!(name: "Mr.Cat", image: "https://media.wired.com/photos/5cdefb92b86e041493d389df/master/pass/Culture-Grumpy-Cat-487386121.jpg", age: "8", sex: "Male", description: "Has Russian Accent", status: 0)
+
+      visit "/pets"
+
+      within ".pets-#{pet1.id}" do
+        click_link "Update Pet"
+      end
+      expect(current_path).to eq("/pets/#{pet1.id}/edit")
+
+      fill_in 'name',  with:     "Mrs Bigglesworth"
+
+      click_on "Update Pet"
+
+      expect(current_path).to eq("/pets/#{pet1.id}")
+      expect(page).to have_content("Mrs Bigglesworth")
+      expect(page).to have_content(pet1.age)
+      expect(page).to have_content(pet1.sex)
+      expect(page).to have_content(pet1.status)
+      expect(page).to have_content(pet1.description)
     end
   end
 end
