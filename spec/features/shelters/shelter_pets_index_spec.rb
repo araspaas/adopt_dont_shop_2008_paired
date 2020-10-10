34acf32694b88c553b4218c6373e01fa1ed8023c
Austin Aspaas <araspaas@live.com>
2020-10-10 13:32:39 -0600

    Add test to allow a visitor to delete a pet from the shelter pets page

--- spec/features/shelters/shelter_pets_index_spec.rb
+++ spec/features/shelters/shelter_pets_index_spec.rb
@@ -47,4 +47,18 @@ RSpec.describe "Shelter specific index page" do
     expect(page).to have_content(pet1.status)
     expect(page).to have_content(pet1.description)
   end
+  it "I can delete a pet" do
+    shelter_1 = create(:shelter)
+    shelter_2 = create(:shelter)
+
+    pet1 = shelter_1.pets.create!(name: "Bella", image: "https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/dog_cool_summer_slideshow/1800x1200_dog_cool_summer_other.jpg", age: "5", sex: "Female", description: "Bork", status: 0)
+    pet2 = shelter_2.pets.create!(name: "Mr.Cat", image: "https://media.wired.com/photos/5cdefb92b86e041493d389df/master/pass/Culture-Grumpy-Cat-487386121.jpg", age: "8", sex: "Male", description: "Has Russian Accent", status: 0)
+
+    visit "/shelters/#{shelter_1.id}/pets"
+
+    within ".pets-#{pet1.id}" do
+      click_link "Delete Pet"
+    end
+    expect(page).to_not have_content(pet1.name)
+  end
 end
