89530b451753aed0bce57e4183a6a5ee88a8f36d
Curtis Bartell <cbartell980@gmail.com>
2020-10-12 17:23:02 -0600

    Add new and create actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,20 @@
 class UsersController < ApplicationController
 
+  def new
+
+  end
+
+  def create
+    user = User.create(user_params)
+    redirect_to("/users/#{user.id}")
+  end
+
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+  def user_params
+    params.permit(:name, :address, :city, :state, :zip)
   end
 end
