cae13e16c3d9d8e24db5b4fd9d4493fbb2ebdb1e
Curtis Bartell <cbartell980@gmail.com>
2020-10-13 17:33:57 -0600

    Add feature test for user reviews

--- spec/features/reviews/show_spec.rb
+++ spec/features/reviews/show_spec.rb
@@ -6,10 +6,10 @@ describe "As a visitor" do
       shelter1 = create(:shelter)
       shelter2 = create(:shelter)
       user = User.create!(name: "John Doe", address: "123 candy cane lane", city: "Denver", state: "Colorado", zip: "80128")
-      review1 = user.reviews.create({title: "Great Shelter!", rating: "4", content: "I had a great experience here!", image: "https://cdn.pixabay.com/photo/2017/11/15/13/52/bulldog-2952049_960_720.jpg", shelter_id: shelter1.id})
-      review2 = user.reviews.create({title: "Awful Shelter!", rating: "2", content: "I had a horrible experience here!", shelter_id: shelter2.id})
+      review1 = user.reviews.create!({title: "Great Shelter!", rating: "4", content: "I had a great experience here!", image: "https://cdn.pixabay.com/photo/2017/11/15/13/52/bulldog-2952049_960_720.jpg", shelter_id: shelter1.id})
+      review2 = user.reviews.create!({title: "Awful Shelter!", rating: "2", content: "I had a horrible experience here!", shelter_id: shelter2.id})
       visit("/shelters/#{shelter1.id}")
-      
+
       expect(page).to have_content(review1.title)
       expect(page).to have_content(review1.rating)
       expect(page).to have_content(review1.content)
--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -22,5 +22,24 @@ describe "As a visitor" do
       expect(page).to have_content(user2.state)
       expect(page).to have_content(user2.zip)
     end
+    it "I can see every review that the user has written" do
+      shelter1 = create(:shelter)
+      shelter2 = create(:shelter)
+      user = User.create!(name: "John Doe", address: "123 candy cane lane", city: "Denver", state: "Colorado", zip: "80128")
+      review1 = user.reviews.create!({title: "Great Shelter!", rating: "4", content: "I had a great experience here!", image: "https://cdn.pixabay.com/photo/2017/11/15/13/52/bulldog-2952049_960_720.jpg", shelter_id: shelter1.id})
+      review2 = user.reviews.create!({title: "Awful Shelter!", rating: "2", content: "I had a horrible experience here!", shelter_id: shelter2.id})
+      visit("/users/#{user.id}")
+      save_and_open_page
+      expect(page).to have_content(review1.title)
+      expect(page).to have_content(review1.rating)
+      expect(page).to have_content(review1.content)
+      expect(page).to have_xpath("//img[@src='#{review1.image}']")
+      expect(page).to have_content(review1.user.name)
+
+      expect(page).to have_content(review2.title)
+      expect(page).to have_content(review2.rating)
+      expect(page).to have_content(review2.content)
+      expect(page).to have_content(review2.user.name)
+    end
   end
 end
