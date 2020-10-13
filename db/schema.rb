56d31faacd421e168670605935930b9131f3664b
Curtis Bartell <cbartell980@gmail.com>
2020-10-12 18:58:07 -0600

    Create table and migrations for Review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_12_215656) do
+ActiveRecord::Schema.define(version: 2020_10_13_002129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,17 @@ ActiveRecord::Schema.define(version: 2020_10_12_215656) do
     t.integer "status"
     t.bigint "shelter_id"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.string "rating"
+    t.string "content"
+    t.string "image"
+    t.bigint "user_id"
+    t.bigint "shelter_id"
+    t.index ["shelter_id"], name: "index_reviews_on_shelter_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "shelters", force: :cascade do |t|
@@ -45,4 +56,6 @@ ActiveRecord::Schema.define(version: 2020_10_12_215656) do
   end
 
   add_foreign_key "pets", "shelters"
+  add_foreign_key "reviews", "shelters"
+  add_foreign_key "reviews", "users"
 end
