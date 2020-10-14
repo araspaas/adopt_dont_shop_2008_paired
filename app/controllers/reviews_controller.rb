f847b3ef773cf374220734cbcaae20551eb69248
Curtis Bartell <cbartell980@gmail.com>
2020-10-13 19:19:41 -0600

    Add edit and update actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,6 +11,17 @@ class ReviewsController < ApplicationController
     redirect_to "/shelters/#{@shelter.id}"
   end
 
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    review = Review.find(params[:id])
+    review.update(review_params)
+
+    redirect_to("/shelters/#{review_params[:shelter_id]}")
+  end
+
   private
   def review_params
     params.permit(:title, :rating, :content, :shelter_id)
