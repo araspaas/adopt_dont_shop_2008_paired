dc04bfdafeb1124ea9a01f2d44b0789d48a5c584
Austin Aspaas <araspaas@live.com>
2020-10-15 19:26:53 -0600

    Update average reviw rating method to account for nil value. Add best and worst review method

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,18 @@ class User < ApplicationRecord
   has_many :shelters, through: :reviews
 
   def average_review_rating
-    reviews.average(:rating)
+    avg = reviews.average(:rating)
+    if avg.nil?
+      avg = 0
+    end
+    avg
+  end
+
+  def best_review
+    reviews.order(rating: :desc).limit(1).first
+  end
+
+  def worst_review
+    reviews.order(:rating).limit(1).first
   end
 end
