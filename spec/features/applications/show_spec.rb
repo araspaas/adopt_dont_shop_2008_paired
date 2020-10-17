caba9387cf5b3e4d8bd53873d544e4b0002f8804
Austin Aspaas <araspaas@live.com>
2020-10-17 16:56:35 -0600

    Add status to pre-created application, Update test to allow user to submit a full application

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -9,7 +9,7 @@ describe "As a visitor" do
       pet2 = shelter.pets.create(image: "https://static.insider.com/image/5d24d6b921a861093e71fef3.jpg", name: "Maisy", age: "6", sex: "female", description: "Stomache on legs", status: 0)
       pet3 = shelter.pets.create(image: "https://media.wired.com/photos/5cdefb92b86e041493d389df/master/pass/Culture-Grumpy-Cat-487386121.jpg", name: "Mr. Cat", age: "9", sex: "male", description: "Has Russian accent", status: 0)
       pet4 = shelter.pets.create(image: "https://i.dailymail.co.uk/1s/2020/03/20/13/26207684-0-image-a-4_1584711978894.jpg", name: "Frank", age: "3", sex: "male", description: "Danger Noodle", status: 0)
-      application = user.applications.create(description: "I'm awesome, give me animals.")
+      application = user.applications.create(description: "I'm awesome, give me animals.", status: 1)
       application.application_pets.create([{pet_id: pet1.id}, {pet_id: pet2.id}, {pet_id: pet3.id}, {pet_id: pet4.id}])
 
       visit "/applications/#{application.id}"
@@ -69,6 +69,20 @@ describe "As a visitor" do
         within "#app-pets-#{pet.name}" do
           expect(page).to have_content(pet.name)
         end
+
+        expect(page).to have_field("Why Are You A Good Owner?")
+
+        fill_in "Why Are You A Good Owner?", with: "Just cuz"
+
+        click_button "Submit Application"
+
+        expect(current_path).to eq("/applications/#{application.id}")
+
+        expect(page).to have_content("pending")
+
+        expect(page).to_not have_field("Add a Pet to this Application:")
+        expect(page).to_not have_button("Adopt This Pet")
+        expect(page).to_not have_field("Why Are You A Good Owner?")
       end
     end
   end
