6b327a318222241512a9d64d439d1166808c4771
Curtis Bartell <cbartell980@gmail.com>
2020-10-17 10:46:34 -0600

    Add #user_name, #full_address, and #pet_names

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,4 +2,17 @@ class Application < ApplicationRecord
   belongs_to :user
   has_many :application_pets
   has_many :pets, through: :application_pets
+  enum status: [:in_progress, :pending, :accepted, :rejected]
+
+  def user_name
+    user.name
+  end
+
+  def full_address
+    "#{user.address}, #{user.city}, #{user.state} #{user.zip}"
+  end
+
+  def pet_names
+    pets.pluck(:name)
+  end
 end
