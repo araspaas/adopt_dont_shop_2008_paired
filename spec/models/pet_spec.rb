8e1d671c804579fea4a74306e6a01e023bba9c43
Curtis Bartell <cbartell980@gmail.com>
2020-10-17 14:49:27 -0600

    Add model test for ::search

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -13,4 +13,18 @@ describe Pet, type: :model do
     it { should have_many :application_pets }
     it { should have_many(:applications).through(:application_pets) }
   end
+
+  describe "class methods" do
+    it "::search" do
+      shelter = Shelter.create(name: "Van's pet shop", address: "3724 tennessee dr", city: "Rockford", state: "Illinois", zip: "61108")
+      pet1 = shelter.pets.create(image: "https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/dog_cool_summer_slideshow/1800x1200_dog_cool_summer_other.jpg", name: "Bella", age: "5", sex: "female", description: "Fun Loving Dog", status: 0)
+      pet2 = shelter.pets.create(image: "https://static.insider.com/image/5d24d6b921a861093e71fef3.jpg", name: "Maisy", age: "6", sex: "female", description: "Stomache on legs", status: 0)
+      pet3 = shelter.pets.create(image: "https://media.wired.com/photos/5cdefb92b86e041493d389df/master/pass/Culture-Grumpy-Cat-487386121.jpg", name: "Mr. Cat", age: "9", sex: "male", description: "Has Russian accent", status: 0)
+      pet4 = shelter.pets.create(image: "https://i.dailymail.co.uk/1s/2020/03/20/13/26207684-0-image-a-4_1584711978894.jpg", name: "Frank", age: "3", sex: "male", description: "Danger Noodle", status: 0)
+      pet5 = shelter.pets.create(image: "https://i.dailymail.co.uk/1s/2020/03/20/13/26207684-0-image-a-4_1584711978894.jpg", name: "Frank", age: "7", sex: "female", description: "Noodle", status: 0)
+
+      expect(Pet.search(pet1.name)).to eq([pet1])
+      expect(Pet.search(pet4.name)).to eq([pet4, pet5])
+    end
+  end
 end
