bb9774d072ed70f8cf3924954df4204b37b88627
Curtis Bartell <cbartell980@gmail.com>
2020-10-19 18:13:02 -0600

    Add #application_approved?

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,7 +2,7 @@ class Application < ApplicationRecord
   belongs_to :user
   has_many :application_pets
   has_many :pets, through: :application_pets
-  enum status: [:in_progress, :pending, :accepted, :rejected]
+  enum status: [:in_progress, :pending, :approved, :rejected]
 
   def user_name
     user.name
@@ -14,5 +14,9 @@ class Application < ApplicationRecord
 
   def pet_names
     pets.select(:id, :name)
+  end
+
+  def application_approved?
+    application_pets.pluck(:status).all?('accepted')
   end
 end
