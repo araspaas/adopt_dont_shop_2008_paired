1d3ae08e5ad7e8d4cb4ae1cb68309b1d48ece781
Austin Aspaas <araspaas@live.com>
2020-10-21 15:00:20 -0600

    Update strong params for a default image for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -35,7 +35,12 @@ class ReviewsController < ApplicationController
 
   private
   def review_params
-    params.permit(:title, :rating, :content, :image, :shelter_id, :user_id)
+    default_image = "https://media3.giphy.com/media/o0vwzuFwCGAFO/giphy.gif?cid=790b761143794a0c3a7b311e04b4bbf2966d12685520db43&rid=giphy.gif"
+    if params[:image] != ""
+      params.permit(:title, :rating, :content, :image, :shelter_id, :user_id)
+    else
+      params.permit(:title, :rating, :content, :shelter_id, :user_id).merge({image: default_image})
+    end
   end
 
   def shelter
@@ -48,6 +53,9 @@ class ReviewsController < ApplicationController
 
   def save_review(user)
     @review = user.reviews.new(review_params)
+    if review_params[:image] == ""
+      review_params[:image] = default_image
+    end
     if @review.save
       redirect_to "/shelters/#{params[:shelter_id]}"
     else
